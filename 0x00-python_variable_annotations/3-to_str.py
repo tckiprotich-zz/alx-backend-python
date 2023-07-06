eba8a1a95957dfb62896b7221e59ca9386daa978
COLLINS  TONUI <tckiprotich@gmail.com>
2023-07-06 12:29:44 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that provides a function to convert a float to a string representation"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string representation
+
+    Args:
+        n (float): The float to be converted
+
+    Returns:
+        str: The string representation of the float
+    """
+    return str(n)
