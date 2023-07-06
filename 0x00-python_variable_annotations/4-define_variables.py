fded111a9405f56f192e784598faf302da18877b
COLLINS  TONUI <tckiprotich@gmail.com>
2023-07-06 12:36:50 +0000

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Task 4's module.
+
+Contains a single function, define_variables, which defines and annotates
+four variables with their respective types.
+'''
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
