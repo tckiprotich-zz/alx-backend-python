254ad6f9078e51186a22383ef51e2bdc05154455
COLLINS  TONUI <tckiprotich@gmail.com>
2023-07-06 12:24:19 +0000

    😎 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/bin/usr/env python3	
+'''Task 1's module.
+
+This module contains a type-annotated function.
+
+'''
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
+
+    Args:
+        str1 (str): The first string to concatenate.
+        str2 (str): The second string to concatenate.
+
+    Returns:
+        str: The concatenated string.
+
+    '''
+    return str1 + str2
